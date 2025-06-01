1fc9e3720711bcb0cbd8e5db11ea894127a5c072
Mert Ustun <mertustun033@gmail.com>
2025-06-01 11:28:38 +0300

    feat: Add DTO for task service

---
+++ Entities/DataTransferObjects/TaskItemForGetDto.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public class TaskItemForGetDto
+    {
+            public Guid Id { get; init; }
+            public string Title { get; init; }
+            public string Description { get; init; }
+            public DateTime CreatedAt { get; init; }
+            public DateTime? Deadline { get; init; }
+            public string? Priority { get; init; }
+            public string? Status { get; init; }
+            public string? Assigned { get; init; }
+            public string CreatedByFullName { get; init; }
+        
+
+    }
+}
