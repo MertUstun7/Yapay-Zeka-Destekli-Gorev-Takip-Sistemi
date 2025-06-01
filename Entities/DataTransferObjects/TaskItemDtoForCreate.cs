47d8746550714377c2f329c8f0acf4352c8300a4
Mert Ustun <mertustun033@gmail.com>
2025-06-01 11:28:07 +0300

    feat: Add DTOS for Task service

---
+++ Entities/DataTransferObjects/TaskItemDtoForCreate.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public class TaskItemDtoForCreate
+    {
+        public string? Title { get; init; }
+        public string? Description { get; init; }
+        public DateTime? Deadline { get; init; }
+        public string? Priority { get; init; }
+
+        public string? AssignedToId { get; init; }
+
+    }
+
+}
