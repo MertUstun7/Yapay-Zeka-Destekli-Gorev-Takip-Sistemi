7d08c61533447b5e95a8a4dbc2c19f61e0b27a85
Mert Ustun <mertustun033@gmail.com>
2025-06-02 11:44:14 +0300

    feat: implement report repository interface

---
+++ Repositories/Contracts/IReportRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts
+{
+    public interface IReportRepository:IGTSBase<TaskReport>
+    {
+
+        Task<List<TaskReport>> GetReportByIdAsync(Guid taskId, bool trackChanges);
+        Task<List<TaskReport>> GetReportByUserIdAsync(string userId, bool trackChanges);
+
+        Task<List<TaskReport>> GetReportsByCompanyIdAsync(Guid companyId, bool trackChanges);
+
+    }
+}
