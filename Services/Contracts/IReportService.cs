46dd429d07b2768dd9cf2a785000b75a58abb509
Mert Ustun <mertustun033@gmail.com>
2025-06-02 11:43:38 +0300

    feat : implement report service interface

---
+++ Services/Contracts/IReportService.cs
@@ -0,0 +1,22 @@
+﻿using Entities.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IReportService
+    {
+        Task<ReportDtoForGet> CreateReportAsync(ReportDtoForCreate dto);
+        Task<IEnumerable<ReportDtoForGet>> GetReportsByTaskIdAsync(Guid taskId);
+        Task<IEnumerable<ReportDtoForGet>> GetMyReportsAsync();
+
+        Task<IEnumerable<ReportDtoForGet>> GetReportsByCompanyAsync();
+
+        Task<ReportFileDto> GetReportFileAsync(Guid id);
+
+        Task DeleteTaskAsync(Guid id);
+    }
+}
