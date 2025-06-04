9e48dd47791a4e28a6a4e1566763111ae65de40a
Mert Ustun <mertustun033@gmail.com>
2025-06-04 11:02:39 +0300

    feat: add logger service

---
+++ Services/Contracts/ILoggerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface ILoggerService
+    {
+        Task LogInfo(string message);
+        Task LogWarning(string message);
+        Task LogError(string message);
+        Task LogDebug(string message);
+
+    }
+}
