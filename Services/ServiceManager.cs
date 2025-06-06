19c16bd76941fe2d1b098e23d17c8a7d92cf8ca0
Mert Ustun <mertustun033@gmail.com>
2025-06-06 19:58:04 +0300

    feat: add service manager service

---
+++ Services/ServiceManager.cs
@@ -0,0 +1,32 @@
+﻿using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    // Presentation katmanında servislere ulaşmamızı sağlayan yapıdır.
+    public class ServiceManager : IServiceManager
+    {
+        public IUserService UserService { get; }
+
+        public ITaskService TaskItemService { get; }
+        public IAuthenticationService AuthenticationService { get; }
+
+        public IReportService ReportService { get; }
+
+        public ServiceManager(ITaskService taskService, IUserService userService, IAuthenticationService authenticationService, IReportService reportService)
+        {
+            UserService = userService;
+
+            AuthenticationService = authenticationService;
+
+            TaskItemService = taskService;
+            ReportService = reportService;
+        }
+
+    }
+}
